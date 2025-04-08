a24190d3d4f0a4e4b229e95219ec9b772d7fe26a
Sanpaphat Porntongprasert <sanpaphat14@gmail.com>
2025-04-08 16:35:40 +0700

    Vercel deploy

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,8 +1,10 @@
+import Image from 'next/image' ;
+
 export default function About(){
     return(
         <div className="wrapper items-center">
-            <p className="text-4xl bold m-4">It doen't have anything in here</p>
-            <img src="img1.png" className="Image1"/>
+            <p className="text-4xl bold m-4">It doesn&apost have anything in here</p>
+            <Image src="/public/img1.png" alt="Image1 "className="Image1"/>
         </div>
     )
 }
--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,8 +1,9 @@
+import Image from 'next/image' ;
 export default function Contact(){
     return(
         <div className="wrapper">
             <p className="text-4xl bold m-4">Contact</p>
-            <img src="img2.png"className="Image1"/>
+            <img src="/public/img2.png" alt="Image2 " className="Image1"/>
         </div>
     )
 }
